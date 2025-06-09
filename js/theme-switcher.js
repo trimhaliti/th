653c9b671f5e4a1f841684e3f155152a0b740be7
trimhaliti <trimhaliti@gmail.com>
2025-06-09 18:08:05 +0200

    Add files via upload

---
+++ js/theme-switcher.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const switcher = document.getElementById('theme-switcher');
+    const moonIcon = switcher.querySelector('.moon-icon');
+    const sunIcon = switcher.querySelector('.sun-icon');
+    const themeSwitcherClickSound = document.getElementById('themeSwitcherClickSound');
+    if (themeSwitcherClickSound) {
+        themeSwitcherClickSound.volume = 0.4;
+    }
+
+    function updateThemeIcon(isDark) {
+        moonIcon.style.display = isDark ? 'none' : 'inline-block';
+        sunIcon.style.display = isDark ? 'inline-block' : 'none';
+    }
+
+    function applyTheme(isDark) {
+        if (isDark) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+        updateThemeIcon(isDark);
+    }
+
+    const storedTheme = localStorage.getItem('theme');
+    applyTheme(storedTheme === 'dark');
+
+    function toggleTheme() {
+        const isDark = !document.documentElement.classList.contains('dark');
+        applyTheme(isDark);
+        localStorage.setItem('theme', isDark ? 'dark' : '');
+
+        if (themeSwitcherClickSound) {
+            themeSwitcherClickSound.play();
+        }
+
+        const currentTheme = isDark ? 'dark' : 'light';
+        document.body.classList.add('theme-transition');
+
+    }
+
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+    if (switcher) {
+        switcher.addEventListener(isTouchDevice ? 'touchstart' : 'click', toggleTheme, {
+            passive: true
+        });
+    }
+});
